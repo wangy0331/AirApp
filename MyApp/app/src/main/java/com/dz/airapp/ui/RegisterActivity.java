d354171c26e4b0ca1e98ab56c27f93ee60a73d7a
aaron.wang <aaron.wang@icitymobile.com>
2016-04-15 16:07:00 +0800

    添加gitignre

--- MyApp/app/src/main/java/com/dz/airapp/ui/RegisterActivity.java
+++ MyApp/app/src/main/java/com/dz/airapp/ui/RegisterActivity.java
@@ -30,6 +30,7 @@ public class RegisterActivity extends BaseActivity {
     private TextView mCodeBtn;
     private Button mRegister;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
